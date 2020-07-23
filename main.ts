9e9af62c773f70a455d96c5ff163a0810274cb56
vghnassia <67999131+vghnassia@users.noreply.github.com>
2020-07-23 17:43:09 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
 let a = 0
 let b = 0
-basic.forever(function () {
+let x = 0
+function lumiere () {
     a = randint(0, 4)
     b = randint(0, 4)
-    for (let index = 0; index < 8; index++) {
-        basic.pause(100)
-        led.plot(a, b)
-        led.plotBrightness(a, b - 1, 150)
-        led.plotBrightness(a, b - 2, 50)
-        led.unplot(a, b - 3)
-        b += 1
+    led.plot(a, b)
+    led.plotBrightness(a, b - 1, 150)
+    led.plotBrightness(a, b - 2, 50)
+}
+basic.forever(function () {
+    lumiere()
+    x = 0
+    for (let x = 0; x <= 4; x++) {
+        let y = 0
+        x = 0
+        if (led.pointBrightness(x, y) == 255) {
+            led.plotBrightness(x, y, 150)
+            led.plot(x, y + 1)
+        } else if (led.pointBrightness(x, y) == 150) {
+            led.plotBrightness(x, y, 50)
+        } else if (led.pointBrightness(x, y) == 50) {
+            led.unplot(x, y)
+        } else {
+        	
+        }
     }
+    basic.pause(500)
 })
